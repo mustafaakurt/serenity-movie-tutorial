dcf10bfb844f61c5b09d7f339565e263dfda9b96
Mustafa Kurt <mustafakurt2000@gmail.com>
2024-02-26 16:42:18 +0300

    customizing movie ui

--- MovieTutorial/MovieTutorial.Web/Modules/MovieDB/Movie/MovieForm.cs
+++ MovieTutorial/MovieTutorial.Web/Modules/MovieDB/Movie/MovieForm.cs
@@ -1,4 +1,4 @@
-﻿using Serenity.ComponentModel;
+using Serenity.ComponentModel;
 using System;
 
 namespace MovieTutorial.MovieDB.Forms;
@@ -8,9 +8,12 @@ namespace MovieTutorial.MovieDB.Forms;
 public class MovieForm
 {
     public string Title { get; set; }
+    [TextAreaEditor(Rows = 3)]
     public string Description { get; set; }
+    [TextAreaEditor(Rows = 3)]
     public string Storyline { get; set; }
     public int Year { get; set; }
     public DateTime ReleaseDate { get; set; }
+    [DisplayName("Runtime in Minutes"), Width(150), AlignRight]
     public int Runtime { get; set; }
 }
