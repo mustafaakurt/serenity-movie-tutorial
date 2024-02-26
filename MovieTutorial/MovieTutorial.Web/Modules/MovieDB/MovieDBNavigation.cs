7ada2caeae560d929cd1fa5aa92cf703af196c26
Mustafa Kurt <mustafakurt2000@gmail.com>
2024-02-26 16:56:30 +0300

    handling-movie-navigation added

--- MovieTutorial/MovieTutorial.Web/Modules/MovieDB/MovieDBNavigation.cs
+++ MovieTutorial/MovieTutorial.Web/Modules/MovieDB/MovieDBNavigation.cs
@@ -1,4 +1,6 @@
-﻿using Serenity.Navigation;
+using Serenity.Navigation;
 using MyPages = MovieTutorial.MovieDB.Pages;
 
-[assembly: NavigationLink(int.MaxValue, "MovieDB/Movie", typeof(MyPages.MoviePage), icon: null)]
+[assembly: NavigationMenu(6000, "Movie Database", icon: "fa-film")]
+[assembly: NavigationLink(6100, "Movie Database/Movies",
+    typeof(MyPages.MoviePage), icon: "fa-video-camera")]
